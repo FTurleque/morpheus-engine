2fa35e27684d5338ae04cd9e8576862ea862ea67
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-22 20:07:57 +0200

    M2 add requirement delta kind

---
+++ morpheus-domain/src/main/java/com/morpheus/domain/requirement/RequirementDeltaKind.java
@@ -0,0 +1,8 @@
+package com.morpheus.domain.requirement;
+
+/** Structural effect declared by a change on a requirement, without M3 temporal projection. */
+public enum RequirementDeltaKind {
+    ADDED,
+    MODIFIED,
+    REMOVED
+}
