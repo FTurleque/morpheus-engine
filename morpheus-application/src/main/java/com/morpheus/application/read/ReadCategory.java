fd8d69494da4587429c78ad60f6ca3c11dec1feb
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-22 21:38:04 +0200

    M2 add unified read categories

---
+++ morpheus-application/src/main/java/com/morpheus/application/read/ReadCategory.java
@@ -0,0 +1,16 @@
+package com.morpheus.application.read;
+
+/** Provider-neutral categories that can be requested from a specification content reader. */
+public enum ReadCategory {
+    CURRENT_SPECIFICATIONS,
+    REQUIREMENTS,
+    SCENARIOS,
+    CHANGES,
+    REQUIREMENT_DELTAS,
+    CONSTRAINTS,
+    DESIGN_DECISIONS,
+    IMPLEMENTATION_TASKS,
+    ACCEPTANCE_CRITERIA,
+    EXTERNAL_REFERENCES,
+    ARCHIVES
+}
