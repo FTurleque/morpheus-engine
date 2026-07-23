3c358a1614c7e8dcd0f9ff918e5a83734c6bf814
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-23 11:59:46 +0200

    M4-S1 add traceability link origin

---
+++ morpheus-domain/src/main/java/com/morpheus/domain/traceability/TraceabilityLinkOrigin.java
@@ -0,0 +1,8 @@
+package com.morpheus.domain.traceability;
+
+/** How MORPHEUS obtained one traceability observation. */
+public enum TraceabilityLinkOrigin {
+    EXPLICIT,
+    DERIVED,
+    HEURISTIC
+}
