55973146eb2a0602d79a33801180af1d318f6325
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-24 09:22:33 +0200

    M8: add requirement change fields

---
+++ morpheus-application/src/main/java/com/morpheus/application/analysis/RequirementChangeField.java
@@ -0,0 +1,11 @@
+package com.morpheus.application.analysis;
+
+/** Documentary dimensions that can differ between the CURRENT baseline and one proposed requirement delta. */
+public enum RequirementChangeField {
+    PRESENCE,
+    SPECIFICATION,
+    KEY,
+    TITLE,
+    STATEMENT,
+    SCENARIOS
+}
