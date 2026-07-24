850c90fc2d497144f4453783042d61b47a15437d
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-24 09:22:39 +0200

    M8: add dependency impact direction

---
+++ morpheus-application/src/main/java/com/morpheus/application/analysis/DependencyImpactDirection.java
@@ -0,0 +1,7 @@
+package com.morpheus.application.analysis;
+
+/** Direction of an explicit DEPENDS_ON path relative to one directly changed requirement. */
+public enum DependencyImpactDirection {
+    DEPENDENCY,
+    DEPENDENT
+}
