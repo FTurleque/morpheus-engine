8bbe9fb0bf3506be3dfe15609f0c5c70d492e637
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-26 17:48:48 +0200

    feat(m18): add provider entity kinds

---
+++ morpheus-application/src/main/java/com/morpheus/application/composition/ProviderEntityKind.java
@@ -0,0 +1,8 @@
+package com.morpheus.application.composition;
+
+/** Provider-neutral entity kinds reconciled by M18 composition. */
+public enum ProviderEntityKind {
+    SPECIFICATION,
+    REQUIREMENT,
+    SCENARIO
+}
